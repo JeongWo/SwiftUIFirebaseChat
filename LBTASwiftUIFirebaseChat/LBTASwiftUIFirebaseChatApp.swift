e36650c8899dcba01bd6815d496c8a934e7b4f77
jeongwookim <112370814+JeongWo@users.noreply.github.com>
2023-04-26 17:04:11 +0900

    Initial Commit

---
+++ LBTASwiftUIFirebaseChat/LBTASwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LBTASwiftUIFirebaseChatApp.swift
+//  LBTASwiftUIFirebaseChat
+//
+//  Created by 김정우 on 2023/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LBTASwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
